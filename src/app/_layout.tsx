a4a89c7e6dec5427f1dda4633028dae35d5a247e
rrascon-gainnoval <rrascon@grupoalta.com>
2026-07-22 16:38:21 -0700

    feat: add react-native-reusables

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,14 @@
 import "../../global.css";
 import { Stack } from "expo-router";
+import { PortalHost } from "@rn-primitives/portal";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      {/* PortalHost Needs to be last child of your providers
+      https://reactnativereusables.com/docs/installation/manual */}
+      <PortalHost />
+    </>
+  );
 }
