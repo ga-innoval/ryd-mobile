76ff6c5ceefea615ccad7afe41088fbf99c7f5bf
rrascon-gainnoval <rrascon@grupoalta.com>
2026-07-22 15:41:33 -0700

    chore: setup expo-router

--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,4 +1,1 @@
 /// <reference types="nativewind/types" />
-
-// Add this to fix the ts(2882) error:
-declare module "*.css";
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-600 m-10">
+        Open up App.tsx to start working on your app!
+      </Text>
+    </View>
+  );
+}
