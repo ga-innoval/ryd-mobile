2d8d7ecde5400fdb2bf74991700a4928e5239dff
rrascon-gainnoval <rrascon@grupoalta.com>
2026-08-10 16:09:21 -0700

    test: add jest setup for react-native-reanimated

--- jest.config.js
+++ jest.config.js
@@ -27,4 +27,5 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|standard-navigation|@rn-primitives)",
   ],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
 };
