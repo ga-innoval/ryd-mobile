ef6602eb3cafca4ec4f9276a09c665afd18df6f1
rrascon-gainnoval <rrascon@grupoalta.com>
2026-08-13 09:24:53 -0700

    tests: add filterAndMatchData suit

--- src/domains/evals/lib/__tests__/filter-data.test.ts
+++ src/domains/evals/lib/__tests__/filter-data.test.ts
@@ -1,5 +1,5 @@
 import { buildEvaluacion } from "@/test-utils/factories/eval.factory";
-import { filterData } from "../filter-data";
+import { filterAndMatchData, filterData } from "../filter-data";
 
 describe("filterData", () => {
   const data = [
@@ -25,5 +25,32 @@ describe("filterData", () => {
 
   it("retorns empty if no matches found", () => {
     expect(filterData(data, "xyz")).toHaveLength(0);
+  });
+});
+
+describe("filterAndMatchData", () => {
+  const data = [
+    buildEvaluacion({ name: "1004-freedom", campo: "Pozo Manuel" }),
+    buildEvaluacion({ name: "1005", campo: "Don Mario" }),
+  ];
+
+  it("retorns every item when query is empty", () => {
+    expect(filterAndMatchData(data, "")).toHaveLength(2);
+  });
+
+  it("filters by partially matched name", () => {
+    expect(filterAndMatchData(data, "1004")).toHaveLength(1);
+  });
+
+  it("filters ignoring upper case and diacritics", () => {
+    expect(filterAndMatchData(data, "PÓZO MANUEL")).toHaveLength(1);
+  });
+
+  it("filters by campo different to name", () => {
+    expect(filterAndMatchData(data, "manuel")).toHaveLength(1);
+  });
+
+  it("retorns empty if no matches found", () => {
+    expect(filterAndMatchData(data, "xyz")).toHaveLength(0);
   });
 });
